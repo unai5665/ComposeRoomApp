4120c5c9a45c1a8f476ffbb2bdcfbe86af4a5f27
unai <unaiperez5665@gmail.com>
2024-11-18 08:15:54 +0000

    importados elementos ui de compose y coroutines

--- app/src/main/java/org/iesharia/composeroomapp/MainActivity.kt
+++ app/src/main/java/org/iesharia/composeroomapp/MainActivity.kt
@@ -5,6 +5,12 @@ import androidx.compose.runtime.Composable
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
 import org.iesharia.composeroomapp.AppDatabase
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.Dispatchers
 
 
 class MainActivity : ComponentActivity() {
