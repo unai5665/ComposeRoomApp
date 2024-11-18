5a94bc3e2783a5fe106f41aae52354323e45dfe8
unai <unaiperez5665@gmail.com>
2024-11-18 08:01:43 +0000

    Definida la base de datos en AppDatabase.kt

--- app/src/main/java/org/iesharia/composeroomapp/AppDatabase.kt
+++ app/src/main/java/org/iesharia/composeroomapp/AppDatabase.kt
@@ -1,2 +1,29 @@
 package org.iesharia.composeroomapp
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Task::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun taskDao(): TaskDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "task_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
